8997730b422623fc91e854769c447be834c22f00
jwk <jwk@clemson.edu>
2018-03-28 19:23:08 -0400

    fixing little things #9

--- files/home_page.php
+++ files/home_page.php
@@ -22,7 +22,7 @@ include("templates/page_template.php");
     <div class="block">
 		
 		<div class="photo">
-			<a href="user_page.php">Senate Hearing</a>
+			<a style="float:right" href="user_page.php">Senate Hearing</a>
 			<img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Senatefloor.jpg" alt="Senate Hearing" width="100" height="60">
 			</img>
 		</div>
