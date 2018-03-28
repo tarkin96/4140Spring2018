974131bdf7c53083056d5c2bb4e9f189eb1bc356
jwk <jwk@clemson.edu>
2018-03-28 17:37:07 -0400

    changed css #10

--- files/home_page.php
+++ files/home_page.php
@@ -1,15 +1,10 @@
 <?php
 session_start();
-echo "this is the homepage"
 ?>
 
 <html>
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<title>Twitch Does Town Hall</title>
   
@@ -18,15 +13,10 @@ echo "this is the homepage"
 
 	<link rel="stylesheet"
 	href="mystyles.css">
-	
-	<link rel="stylesheet" href="css/bootstrap.min.cs">
 
 </head>
 
 <body style="background-color:#444">
-	
-<script src="js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 <div class="topnav">
 	<a class="active" href="home_page.php">Home</a>
