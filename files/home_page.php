fec69368aefe825d3ac0d3cd615dc87c2d72b913
jwk <jwk@clemson.edu>
2018-03-29 07:20:48 -0400

    fixing stream and chat #24

--- files/home_page.php
+++ files/home_page.php
@@ -16,7 +16,7 @@ include("templates/page_template.php");
 
 </head>
 
-<div id="middle">
+<div class="middle">
 	<div class="vertical-menu">
 		<a href="#recserv" class="cascade">Recommended Services</a>
 		<div class="block">
