ca65497aa0ad3e6afd3b40e3deba71707594aa07
jwk <jwk@clemson.edu>
2018-03-29 20:09:09 -0400

    working on chat # 6

--- files/js/chat.js
+++ files/js/chat.js
@@ -1,7 +1,7 @@
 	//If user submits the form
 	$("#submitmsg").click(function(){	
 		var clientmsg = $("#usermsg").val();
-		$.post("../chat.php", {text: clientmsg});				
+		$.post("chat.php", {text: clientmsg}, function(data){ console.log(data);});				
 		$("#usermsg").attr("value", "");
 		return false;
 	});
