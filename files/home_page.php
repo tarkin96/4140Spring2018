b495907679ade076f36e282eb84480a5a19eef1c
jwk <jwk@clemson.edu>
2018-03-29 22:55:43 -0400

    fixed little issues #3

--- files/home_page.php
+++ files/home_page.php
@@ -124,6 +124,12 @@ include("config.php");
 			<desc>Next: Mark Zuckerberg interrogation</desc>
 		</div>
 	</div>
+	
+	<div class="questions">
+		<desc><b>Top Viewer Concerns</b><br><br></desc>
+		<desc>What charges are being considered against Zuckerberg?<br><br><br></desc>
+		<desc>We need to address online privacy concerns.  What is congress going to do about it?</desc>
+    </div>
 
 	<div class="poll">
 	  <form method="post" action="https://poll.pollcode.com/17834677"><div style="background-color:#999;padding:2px;width:175px;font-family:Arial;font-size:small;color:#000;"><div style="padding:2px 0px 4px 2px;"><strong>Do you like pie?</strong></div><input type="radio" name="answer" value="1" id="answer178346771" style="float:left;" /><label for="answer178346771" style="float:left;width:150px;">Yes</label><div style="clear:both;height:2px;"></div><input type="radio" name="answer" value="2" id="answer178346772" style="float:left;" /><label for="answer178346772" style="float:left;width:150px;">No</label><div style="clear:both;height:2px;"></div><div align="center" style="padding:3px;"><input type="submit" value=" Vote ">&nbsp;<input type="submit" name="view" value=" View "></div><div align="right" style="font-size:10px">pollcode.com <a href="https://pollcode.com/">free polls</a></div></div></form>
