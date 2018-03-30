258c45d360879275ecce189513801aacc144ccc5
jwk <jwk@clemson.edu>
2018-03-29 23:45:54 -0400

    fixed little issues #16

--- files/user_page2.php
+++ files/user_page2.php
@@ -1,12 +1,9 @@
 <?php
 include("templates/page_template.php");
 include("config.php");
-
-//function add_to_chat() {
-	//include("config.php");
-	//$chat_insert_query = "INSERT INTO `Studio_Day_Chat` VALUES ('$_SESSION['login_user']', $text);
-	//$result = mysqli_query($db,$chat_insert_query);
-//}
+if (array_key_exists('login_user', $_SESSION)) {
+	header("Location: home_page.php");
+}
 ?>
 
 <html>
@@ -22,22 +19,22 @@ include("config.php");
 		<a href="#recserv" class="cascade">Recommended Services</a>
 		<div class="block">
 			<div class="photo">
-				<a class="recstream" href="user_page.php">Senate Hearing</a>
+				<a class="recstream" href="user_page1.php">Senate Hearing</a>
 				<img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Senatefloor.jpg" alt="Senate Hearing" width="100" height="60">
 				</img>
 			</div>
 		</div>
 		<div class="block">
-			<service href="#townhall">Town Hall<br>Meeting</service>
 			<div class="photo">
+				<a class="recstream" href="user_page2.php">Town Hall<br>Meeting</a>
 				<img src="https://media.glassdoor.com/l/44/eb/14/98/town-hall-meeting-in-action.jpg"
 					alt="Town Hall Meeting" width="100" height="60">
 				</img>
 			</div>
 		</div>
 		<div class="block">
-			<service href="#debate">Debate</service>
 			<div class="photo">
+				<a class="recstream" href="user_page3.php">Debate</a>
 				<img src="https://media.npr.org/assets/img/2012/10/03/first_debate1-e2ffc0e8d70fdb7120584d533f027326b05e61c0-s900-c85.jpg"
 					alt="Debate" width="100" height="60">
 				</img>
