c5308fec469708c1de1b98479af165f1c1ae3c2f
jwk <jwk@clemson.edu>
2018-04-02 06:19:48 -0400

    fixing up before turn in

--- files/user_page1.php
+++ files/user_page1.php
@@ -112,13 +112,18 @@ if (!array_key_exists('login_user', $_SESSION)) {
 
 <div class="bott">
 
-	<div class="eventdesc">
-		<desc>The Senate interrogates Mark Zuckerberg in regards to recent data breaches
-		and the nature of user privacy on Facebook. This follows the recent news that Facebook released data on millions of its users 
-		to firms that target swing voters in political campaigns. CEO Mark Zuckerberg defends his innocence, pointing out that users signed away their data
-		rights during signup.</desc>
+	<div>
+		<div class="eventdesc">
+			<desc>The Senate interrogates Mark Zuckerberg in regards to recent data breaches
+			and the nature of user privacy on Facebook. This follows the recent news that Facebook released data on millions of its users 
+			to firms that target swing voters in political campaigns. CEO Mark Zuckerberg defends his innocence, pointing out that users signed away their data
+			rights during signup.</desc>
+		</div>
+		
+		<div class="poll">
+			<form method="post" action="https://poll.pollcode.com/17834677"><div style="background-color:#999;padding:2px;width:175px;font-family:Arial;font-size:small;color:#000;"><div style="padding:2px 0px 4px 2px;"><strong>Do you like pie?</strong></div><input type="radio" name="answer" value="1" id="answer178346771" style="float:left;" /><label for="answer178346771" style="float:left;width:150px;">Yes</label><div style="clear:both;height:2px;"></div><input type="radio" name="answer" value="2" id="answer178346772" style="float:left;" /><label for="answer178346772" style="float:left;width:150px;">No</label><div style="clear:both;height:2px;"></div><div align="center" style="padding:3px;"><input type="submit" value=" Vote ">&nbsp;<input type="submit" name="view" value=" View "></div><div align="right" style="font-size:10px">pollcode.com <a href="https://pollcode.com/">free polls</a></div></div></form>
+		</div>
 	</div>
-
 	<div class="botmiddle">
 		<div class="agenda">
 			<desc>Agenda</desc>
@@ -135,9 +140,7 @@ if (!array_key_exists('login_user', $_SESSION)) {
 	
 
 
-	<div class="poll">
-		<form method="post" action="https://poll.pollcode.com/17834677"><div style="background-color:#999;padding:2px;width:175px;font-family:Arial;font-size:small;color:#000;"><div style="padding:2px 0px 4px 2px;"><strong>Do you like pie?</strong></div><input type="radio" name="answer" value="1" id="answer178346771" style="float:left;" /><label for="answer178346771" style="float:left;width:150px;">Yes</label><div style="clear:both;height:2px;"></div><input type="radio" name="answer" value="2" id="answer178346772" style="float:left;" /><label for="answer178346772" style="float:left;width:150px;">No</label><div style="clear:both;height:2px;"></div><div align="center" style="padding:3px;"><input type="submit" value=" Vote ">&nbsp;<input type="submit" name="view" value=" View "></div><div align="right" style="font-size:10px">pollcode.com <a href="https://pollcode.com/">free polls</a></div></div></form>
-	</div>
+
 </div>
 
 </body>
