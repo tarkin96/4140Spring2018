67dadd7a7e5c9a56c759f716508a812d30e605f2
jwk <jwk@clemson.edu>
2018-04-12 17:51:18 -0400

    fixing up for evaluations #4

--- files/user_page2.php
+++ files/user_page2.php
@@ -60,52 +60,20 @@ $_SESSION['Stream_Chat'] = "`Stream2_Chat`";
 			<p class="welcome">Stream Chat</p>
 			<div style="clear:both"></div>
 		</div>
-<form name="message" method="POST" action="" onsubmit="event.preventDefault(); chat();">
-		<div id="chatbox" action="">
-			<?php
-			/*if (isset($_POST['submitmsg'])) { 
-					//echo "button has been pressed"; 
-				
-				$un = "'" . $_SESSION['login_user'] . "'";
-				//print_r($_POST);
-				$un2 = "'" . $_POST['usermsg'] . "'";
-				//echo "INSERT INTO Studio_Day_Chat VALUES ($un, $un2)";
-				$chat_insert_query = "INSERT INTO `Studio_Day_Chat2` VALUES ($un, $un2)";
-				$result = mysqli_query($db,$chat_insert_query);
 
-			}
-			unset($_POST['usermsg']);
-			unset($_POST['submitmsg']);
-			//if($_SERVER["REQUEST_METHOD"] == "POST") {
-				$grab_chat_query = "SELECT * FROM `Studio_Day_Chat2`";
-				$chat_result = mysqli_query($db,$grab_chat_query);
-				//$chat_rows = mysqli_fetch_array($chat_result,MYSQLI_ASSOC);	
-				//if ($chat_rows) {
-					//foreach($chat_rows as $row) {
-						//echo $row;
-					//}
-				//}
-				if ($chat_result) {
-					while($chat_rows = mysqli_fetch_array($chat_result,MYSQLI_ASSOC))	
-					{
-						echo "<strong>";
-						echo $chat_rows['User_Name'] . " : ";
-						echo "</strong>";
-						echo $chat_rows['Message'];
-						echo "<br>";
-					}
-				}
-			//}//end of if*/
-			?>
-		</div>
-		
-	
-	
-		
+		<form name="message" method="POST" action="" onsubmit="event.preventDefault(); chat();">
+			<div id="chatbox" action="">
+
+			</div>
+		<?php if (array_key_exists('login_user', $_SESSION)) { ?>			
 			<div id="in"> 
 				<input name="usermsg" type="text" class="usermsg" />
 				<input name="submitmsg" type="button"  class="submitmsg" value="Send" onclick="chat();"/>
 			</div>
+		<?php } else { ?>
+				You must login to submit to the chat. 
+
+		<?php } ?>
 		</form>
 	</div>
 
--- files/user_page3.php
+++ files/user_page3.php
@@ -60,53 +60,20 @@ $_SESSION['Stream_Chat'] = "`Stream3_Chat`";
 			<p class="welcome">Stream Chat</p>
 			<div style="clear:both"></div>
 		</div>
-<form name="message" method="POST" action="" onsubmit="event.preventDefault(); chat();">
-		<div id="chatbox" action="">
-			<?php
-			/*if (isset($_POST['submitmsg'])) { 
-					//echo "button has been pressed"; 
-				
-				$un = "'" . $_SESSION['login_user'] . "'";
-				//print_r($_POST);
-				$un2 = "'" . $_POST['usermsg'] . "'";
-				//echo "INSERT INTO Studio_Day_Chat VALUES ($un, $un2)";
-				$chat_insert_query = "INSERT INTO `Studio_Day_Chat3` VALUES ($un, $un2)";
-				$result = mysqli_query($db,$chat_insert_query);
 
-			}
-			unset($_POST['usermsg']);
-			unset($_POST['submitmsg']);
-			//if($_SERVER["REQUEST_METHOD"] == "POST") {
-				$grab_chat_query = "SELECT * FROM `Studio_Day_Chat3`";
-				$chat_result = mysqli_query($db,$grab_chat_query);
-				//$chat_rows = mysqli_fetch_array($chat_result,MYSQLI_ASSOC);	
-				//if ($chat_rows) {
-					//foreach($chat_rows as $row) {
-						//echo $row;
-					//}
-				//}
-				if ($chat_result) {
-					while($chat_rows = mysqli_fetch_array($chat_result,MYSQLI_ASSOC))	
-					{
-						echo "<strong>";
-						echo $chat_rows['User_Name'] . " : ";
-						echo "</strong>";
-						echo $chat_rows['Message'];
-						echo "<br>";
-					}
-				}
+		<form name="message" method="POST" action="" onsubmit="event.preventDefault(); chat();">
+			<div id="chatbox" action="">
 
-			//}//end of if*/
-			?>
-		</div>
-		
-	
-	
-		
+			</div>
+		<?php if (array_key_exists('login_user', $_SESSION)) { ?>			
 			<div id="in"> 
 				<input name="usermsg" type="text" class="usermsg" />
 				<input name="submitmsg" type="button"  class="submitmsg" value="Send" onclick="chat();"/>
 			</div>
+		<?php } else { ?>
+				You must login to submit to the chat. 
+
+		<?php } ?>
 		</form>
 	</div>
 
